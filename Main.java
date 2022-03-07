b6b5b3ec5e49bb92872b8c99d3c82955e0ac8540
Alexey Kudryavtsev <axel_47@mail.ru>
2022-03-07 17:21:11 +0300

    First commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+package Worker;
+
+public class Main {
+
+    public static void main(String[] args) {
+        OnTaskDoneListener listener = System.out::println;
+        Worker worker = new Worker(listener);
+        worker.start();
+
+    }
+}
